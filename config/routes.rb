99de68877d29707bfd2a92cbec2354f35f28d7f5
Dandush03 <d.laloush@outlook.com>
2020-03-06 16:34:01 -0400

    CP-Chapter 3-4

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users
-
+  root 'static_pages#home'
+  get 'static_pages/help'
+  get  'static_pages/about'
+  get  'static_pages/contact'
 end
---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,33 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+  
+  test "should get root" do
+    get root_url
+    assert_response :success
+    assert_select "title", "#{@base_title}"
+  end
+
+  test "should get help" do
+    get static_pages_help_url
+    assert_response :success
+    assert_select "title", "Help | #{@base_title}"
+  end
+  
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+    assert_select "title", "About | #{@base_title}"
+  end
+
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
+  end
+
+end
