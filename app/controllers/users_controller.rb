d862dc7b75277c491ada557a903693c09093dae8
Dandush03 <d.laloush@outlook.com>
2020-03-06 16:12:18 -0400

    finished tutorial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,17 @@
 class UsersController < ApplicationController
   def index
     @name = 'I am the Index action'
-    @user = 'Daniel'
-    @email = 'd.laloush@outlook.com'
-    @country = 'Venezuela'
+  end
+
+  def new
+    @name = 'I am the Index action1'
+  end
+
+  def create
+    @user = User.new(params)
+  end
+
+  def show
+    @name = 'I am the Index action2'
   end
 end
